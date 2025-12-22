6a5479f5e2e188f90bce2955fca974b1ec0c1baa
Dylan Nas <github.swampland@alias.dylannas.nl>
2025-12-23 00:07:30 +0100

    fix: missing get_current_ip.sh (#8)

---
+++ terraform/scripts/utilities/retrieve_current_ip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TIMEOUT=5
+IP=""
+
+if [[ "$1" == "--ipv4" ]]; then
+  IP=$(curl -s -4 --connect-timeout $TIMEOUT https://ipv4.icanhazip.com || true)
+
+elif [[ "$1" == "--ipv6" ]]; then
+  IP=$(curl -s -6 --connect-timeout $TIMEOUT https://ipv6.icanhazip.com || true)
+fi
+
+IP=$(echo "$IP" | tr -d '[:space:]')
+
+printf '{"ip": "%s"}\n' "$IP"
